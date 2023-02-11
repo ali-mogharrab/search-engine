0c7113f7419879a54bc4fdc5fa040d4285a34b55
Ali Mogharrab <alimogharrab101@gmail.com>
2023-02-11 21:54:12 +0330

    Add load_stop_words method

--- src/run.py
+++ src/run.py
@@ -6,9 +6,15 @@ from src.nlp.text_process import (ConvertCase, RemovePunc, RemoveSpace,
 
 
 class Search:
-    def __init__(self, document_path: Union[str, Path]):
+    def __init__(self, document_path: Union[str, Path], stop_words_path: Union[str, Path] = 'data/stop_words.txt'):
         #crawl data
         self.data = self.crawl(document_path)
+
+        # load text processor
+        self.pipe = TextPipeline(ConvertCase, RemovePunc, RemoveSpace)
+
+        # load stop words
+        self.stop_words = self.load_stop_words(stop_words_path)
 
     def crawl(self, document_path: Union[str, Path]) -> dict:
         """
@@ -24,6 +30,22 @@ class Search:
                     data[doc_path] = f.read()
 
         return data
+
+    def load_stop_words(self, stop_words_path: Union[str, Path]) -> set:
+        """
+        Load stop words from file.
+
+        :param stop_words_path: Path to stop words.
+        :return: Set of stop words.
+        """
+        with open(stop_words_path) as f:
+            stop_words = f.read()
+
+        stop_words = stop_words.split('\n')
+        # Process stop words
+        stop_words = set(map(self.pipe.transform, stop_words))
+
+        return stop_words
 
 
 if __name__ == '__main__':
