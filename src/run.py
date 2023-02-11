20e2777d62ffd6d009ebe0ad10c0e14e3c9d9f5e
Ali Mogharrab <alimogharrab101@gmail.com>
2023-02-11 22:48:37 +0330

    Add index_data method

--- src/run.py
+++ src/run.py
@@ -6,15 +6,24 @@ from src.nlp.text_process import (ConvertCase, RemovePunc, RemoveSpace,
 
 
 class Search:
-    def __init__(self, document_path: Union[str, Path], stop_words_path: Union[str, Path] = 'data/stop_words.txt'):
-        #crawl data
-        self.data = self.crawl(document_path)
+    def __init__(self, documents_path: Union[str, Path], stop_words_path: Union[str, Path] = 'data/stop_words.txt'):
+        """
+        Search Engine to search in documents.
+
+        :param documents_path: Path to documents
+        :param stop_words_path: Path to stop words
+        """
+        # crawl data
+        self.data = self.crawl(documents_path)
 
         # load text processor
         self.pipe = TextPipeline(ConvertCase, RemovePunc, RemoveSpace)
 
         # load stop words
         self.stop_words = self.load_stop_words(stop_words_path)
+
+        # index data
+        self.index = self.index_data()
 
     def crawl(self, document_path: Union[str, Path]) -> dict:
         """
@@ -46,6 +55,34 @@ class Search:
         stop_words = set(map(self.pipe.transform, stop_words))
 
         return stop_words
+
+    def index_data(self) -> dict:
+        """
+        Index data.
+
+        return a dict which keys are words of data dict
+        and values are a dict of doc_paths with frequency
+        of the word in doc_path
+
+        :return: Index of data.
+        """
+        index = {}
+        for doc_path, doc_content in self.data.items():
+            for word in doc_content.split():
+                word = self.pipe.transform(word)
+
+                if not word or word in self.stop_words:
+                    continue
+
+                # update words value dict
+                if word in index:
+                    index[word][doc_path] = index[word].get(doc_path, 0) + 1
+
+                # set words value to a dict for the first time
+                else:
+                    index[word] = {doc_path: 1}
+
+        return index
 
 
 if __name__ == '__main__':
